b13e243d48abc52c2542d7b48f058fe25c693aa5
Blue Padge <lijianjian13@gmail.com>
2019-01-26 14:07:48 +0800

    binary search demo

--- src/main/java/blue/padge/main.java
+++ src/main/java/blue/padge/main.java
@@ -1,7 +1,20 @@
 package blue.padge;
 
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello world.");
+        int N = 100;
+        StdDraw.setXscale(0,N);
+        StdDraw.setYscale(0, N * N);
+        StdDraw.setPenRadius(0.01);
+        for (int i = 0; i < N; i++) {
+            StdDraw.point(i, i);
+            StdDraw.point(i, i * i);
+            StdDraw.point(i, i * Math.log(i));
+        }
+
     }
 }
